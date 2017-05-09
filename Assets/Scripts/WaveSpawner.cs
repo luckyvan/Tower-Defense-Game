604cd493df77cf4b5cbbcbfa27a92858fa75cb88
Fan Chou <Chou.Fan@igt.com>
2017-05-09 23:04:21 +0800

    Episode - 3: Wave Spawner.

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    void Update ()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWaves());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private IEnumerator SpawnWaves()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
 }

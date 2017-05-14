e9e306f917223406f7b33eb39da581b9813c56d0
Fan Chou <Chou.Fan@igt.com>
2017-05-14 10:24:27 +0800

    Episode - 4: Turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public Transform target;
+    public float range = 15f;
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    private string enemyTag = "Enemy";
+    void Start ()
+    {
+        InvokeRepeating("UpdateTarget", 0f, .5f);
+	}
+	
+	void Update ()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0, rotation.y, 0);
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(
+                transform.position, 
+                enemy.transform.position
+                );
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+}

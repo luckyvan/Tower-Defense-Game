881d11fc0df600d4914b650f3fe595523ce6cc68
Fan Chou <Chou.Fan@igt.com>
2017-05-14 22:30:44 +0800

    Episode - 6: Building

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More Than 1 BuildManager in Scene");
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    private Color startColor;
+    private GameObject turret;
+
+    private Renderer rend;
+    private Vector3 positionOffset = new Vector3(0, 0.5f, 0);
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't Build there! -- TODO: Display ON Screen");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}

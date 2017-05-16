47d2854d23e5b2d5e79b697dce81f00e6121ce18
Fan Chou <Chou.Fan@igt.com>
2017-05-16 16:49:50 +0800

    Episode - 10 : Missles.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
 
     public GameObject impactPrefab;
 
@@ -32,14 +33,47 @@ public class Bullet : MonoBehaviour {
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 	}
 
     private void HitTarget()
     {
         GameObject effectInstance = GameObject.Instantiate(impactPrefab, transform.position, transform.rotation);
 
-        Destroy(effectInstance, 2f);
+        Destroy(effectInstance, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
 
         Destroy(this.gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, explosionRadius);
+        foreach(var collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -14,9 +14,9 @@ public class Shop : MonoBehaviour {
         buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
     }
 
-    public void PurchaseAnotherTurret()
+    public void PurchaseMissileLauncher()
     {
-        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
     }
 
 }
